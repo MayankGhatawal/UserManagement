02c923be3c154ca7b508acc4fb4dff51331e7ef6
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-10-03 02:00:11 +0530

    main

--- src/middleware/multerMiddleware.js
+++ src/middleware/multerMiddleware.js
@@ -1,28 +1,5 @@
 import multer from "multer";
-import path from "path";
 
-// Storage config
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // folder where files will be saved
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + path.extname(file.originalname) // unique filename
-    );
-  },
-});
+const storage = multer.memoryStorage(); // Store file in memory
 
-// File filter (optional - only allow images)
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpeg|jpg|png|gif/;
-  const ext = path.extname(file.originalname).toLowerCase();
-  if (allowedTypes.test(ext)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only images are allowed"), false);
-  }
-};
-
-export const upload = multer({ storage, fileFilter });
+export const upload = multer({ storage });
