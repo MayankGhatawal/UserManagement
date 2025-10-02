a08a51e0e9728269ede318abc949f06678e9994a
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-10-03 00:24:45 +0530

    main

--- src/server.js
+++ src/server.js
@@ -3,13 +3,18 @@ import connectDB from "./config/db.js";
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
+import userAuth from "./routes/userAuth.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api", userRoutes);
+app.use("/auth", userAuth);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Node.js, which is building basic CRUD APIs for a User resource.");
