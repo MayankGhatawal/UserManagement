2e34b66e0ffb610808973136774070c1c09d17e1
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-10-03 01:27:09 +0530

    main

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/userAuth.js";
+import { upload } from "../middleware/multerMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", upload.single("profilePic"), registerUser);
 router.post("/login", loginUser);
 
 export default router;
